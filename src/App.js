b5cd1fcb6e56d87a90d68589a9e9058c63f351e5
ujjwalsingh996 <ujjwalsingh996@gmail.com>
2023-03-29 18:28:08 +0530

    Login Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import { Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
+import MailBox from "./components/MailBox";
 import SignUp from "./components/SignUp";
+
+
 function App() {
   return (
     <Switch>
       <Route path="/" exact>
         <SignUp />
+      </Route>
+      <Route path="/login">
+        <Login/>
+      </Route>
+      <Route path="/mailbox">
+        <MailBox/>
       </Route>
       </Switch>
   );
---
+++ src/components/MailBox.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MailBox = () => {
+
+    return (
+        <React.Fragment>
+            <h1>Mail Box Client!</h1>
+        </React.Fragment>
+    )
+}
+
+export default MailBox;
